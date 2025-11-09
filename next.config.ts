bba32feb519eb844d0807ea6d99df186b986fa1a
Shinia-Gupta <shiniavit@gmail.com>
2025-11-09 19:40:37 +0530

    modify deprecated nextjs config for image imports

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,14 @@ const nextConfig: NextConfig = {
 
   },
   images:{
-    domains:["avatars.githubusercontent.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   }
 };
 
