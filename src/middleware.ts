1b0a2faee8ee6f2c9305c59a3d52bbb06f360f56
Shinia-Gupta <shiniavit@gmail.com>
2025-11-10 15:34:44 +0530

    modify middleware for accessibility of user-info and login,register pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,26 @@ import { NextRequest } from "next/server";
 import { auth } from "@/src/auth";
 
 const protectedRoutes=["/user-info"]
+const authRoutes=["/login","/signup"]
 
 export default async function middleware(request:NextRequest){
+    //if session - user-info accessible but not login,register page
+    //if not session - login,register accessible but not user-info page
+
     const session=await auth();
 const {pathname}=request.nextUrl;
 
     const isProtected=protectedRoutes.some((route)=>pathname.startsWith(route));
+    const isAuth =authRoutes.some((route)=>pathname.startsWith(route));
 
     if(isProtected && !session){
-        return NextResponse.redirect(new URL("/api/auth/signin",request.url))
+        return NextResponse.redirect(new URL("/login",request.url))
+    }
+     if(isAuth && session){
+        return NextResponse.redirect(new URL("/user-info",request.url))
+
     }
 
     return NextResponse.next();
 }
+
