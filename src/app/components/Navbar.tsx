6b2b56abc00a310e4d4a1dfaf5d8802c42a9f7ff
Shinia-Gupta <shiniavit@gmail.com>
2025-11-12 12:14:48 +0530

    resolve user avatar issue

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default async function Navbar() {
             <>
               <Link href="/dashboard" className="flex items-center gap-2 hover:opacity-80">
                 <Image
-                  src={user.image || "/default-avatar.png"}
+                  src={user.image || "/user-default-avatar.png"}
                   alt="User Avatar"
                   width={36}
                   height={36}
